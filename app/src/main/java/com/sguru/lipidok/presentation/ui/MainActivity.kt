0e874b6a768fe4a0b2fd1e47ee08efc97c1b2a37
sergejmehonosin <sergey01051997@mail.ru>
2024-04-21 17:17:18 +0300

    Добавление Splash Screen

--- app/src/main/java/com/sguru/lipidok/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/sguru/lipidok/presentation/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,12 +17,14 @@ import com.sguru.lipidok.presentation.ui.theme.LipidOkTheme
 
 @SuppressLint("Custom")
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
 
         splashScreen.setKeepOnScreenCondition {
-            true
+            viewModel.isLoading.value
         }
 
 
